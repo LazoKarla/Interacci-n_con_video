696707c14806ff7e64d9a1f62b56cb4226ceb1cb
Karla Lazo <karlalazoot@gmail.com>
2025-01-12 19:18:41 -0500

    Se agregó una descripción

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  background(0); // Fondo negro
+  noLoop();
+
+  drawText();
 }
 
-function draw() {
-  background(220);
+function drawText() {
+  fill(255); // Texto blanco
+  noStroke();
+
+  // Título
+  textFont("Poppins Bold");
+  textSize(60); // Tamaño de la fuente del título
+  textAlign(CENTER, TOP);
+  fill(191, 30, 131); // Color del título (fucsia)
+  text("GALERÍA DE ARTE EN VIVO", width / 2, height * 0.1); // Centrado horizontalmente
+
+  // Descripción
+  textFont("Arial");
+  textSize(28); // Tamaño de la fuente del cuerpo
+  fill(255); // Color del texto (blanco)
+  textAlign(CENTER, TOP);
+  let description =
+    "El proyecto transforma objetos cotidianos en estilos artísticos aplicados en tiempo real a la imagen capturada por la cámara del usuario. Cada objeto clasificado se traduce en un estilo artístico diferente aplicado a la imagen en tiempo real. ";
+
+  let marginX = width * 0.1; // Márgenes laterales del texto
+  let textWidthValue = width - 2 * marginX;
+  text(description, marginX, height * 0.2, textWidthValue, height * 0.5); // Ajuste del ancho del texto y posición
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0); // Fondo negro
+  drawText();
 }
